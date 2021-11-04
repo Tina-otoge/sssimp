cf097eab926ce97cbf875ad24e637d3f4f28359c
Tina <me@tina.moe>
2021-11-04 07:33:37 +0100

    Bump version

--- src/sssimp/__init__.py
+++ src/sssimp/__init__.py
@@ -1,11 +1,10 @@
 import functools
 from pathlib import Path
-import jinja2
 from jinja2 import Environment, FileSystemLoader
 
 from . import config
 
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 APP_DIR = Path(__file__).parent
 INPUT_DIR = Path(config.INPUT_PATH)
