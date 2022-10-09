f81edee90ab695ae60d296e7132d5d426d2f6d12
Gabor Szabo <gabor@szabgab.com>
2022-10-09 13:21:21 +0300

    imporve diff code to show differences recursively

--- test_examples.py
+++ test_examples.py
@@ -10,16 +10,17 @@ import pytest
 root = os.path.dirname(__file__)
 examples = os.listdir(os.path.join(root, 'examples'))
 
-def check_diffs(dcmp, left, right):
+def check_diffs(dcmp):
+    print(f'check_diffs {dcmp.left} and {dcmp.right}')
     assert dcmp.left_only == []
     assert dcmp.right_only == []
     success = True
     for filename in dcmp.diff_files:
         success = False
         print(f"File with difference: {filename}")
-        with open(os.path.join(left, filename)) as fh:
+        with open(os.path.join(dcmp.left, filename)) as fh:
             left_content = fh.readlines()
-        with open(os.path.join(right, filename)) as fh:
+        with open(os.path.join(dcmp.right, filename)) as fh:
             right_content = fh.readlines()
         for row in difflib.unified_diff(left_content, right_content):
             print(row, end="")
@@ -27,7 +28,7 @@ def check_diffs(dcmp, left, right):
 
     #assert dcmp.diff_files == []
     for sub_dcmp in dcmp.subdirs.values():
-        success = success and check_diffs(sub_dcmp, left, right)
+        success = check_diffs(sub_dcmp) and success
     return success
 
 @pytest.mark.parametrize("example", examples)
@@ -49,5 +50,5 @@ def test_examples(example, tmpdir, request):
 
     dcmp = filecmp.dircmp(expected_output, outdir)
     print(expected_output, outdir)
-    assert check_diffs(dcmp, str(expected_output), str(outdir)), "Some files differ. See above using the -s flag of pytest"
+    assert check_diffs(dcmp), "Some files differ. See above using the -s flag of pytest"
 
