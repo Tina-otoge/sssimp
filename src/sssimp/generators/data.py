dfb002b4105eeaf84d44cdc5270656534f70dcd0
Damien Savatier <damien.savatier@visian.fr>
2024-07-07 04:19:44 +0200

    Add markdown data type

--- src/sssimp/generators/data.py
+++ src/sssimp/generators/data.py
@@ -9,6 +9,8 @@ import sssimp
 from sssimp import jinja
 from sssimp.utils import path_strip
 
+from .markdown import MarkdownPage
+
 DATA_DIR = sssimp.INPUT_DIR / "data"
 
 
@@ -17,19 +19,28 @@ class Data:
         self.dict = {}
 
     def handle_file(self, path: Path):
+        def opener(func):
+            def wrapped(path):
+                with path.open() as f:
+                    return func(f)
+
+            return wrapped
+
         parents = [
             x.split(".")[0]
             for x in path_strip(path, DATA_DIR).split(os.path.sep)
         ]
         getter = {
-            "json": json.load,
-            "yaml": yaml.safe_load,
-            "yml": yaml.safe_load,
+            "md": read_markdown,
+            "json": opener(json.load),
+            "yaml": opener(yaml.safe_load),
+            "yml": opener(yaml.safe_load),
         }.get(path.suffix[1:])
         if not getter:
+            logging.warning(f"Unsupported data file {path}")
             return
-        with open(path) as f:
-            data = getter(f)
+        data = getter(path)
+        logging.debug(f"Data: {path} -> {data}")
         target = self.dict
         for parent in parents[:-1]:
             target = target.setdefault(parent, {})
@@ -42,3 +53,8 @@ def prepare():
         logging.info(f"Handling data file {file}")
         data.handle_file(file)
     jinja.globals["data"] = data.dict
+
+
+def read_markdown(path: Path):
+    logging.info("read_markdown")
+    return MarkdownPage(path, path)
